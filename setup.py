320af9e6c67443f7802ed4add6598e968aaf2191
Nick Whyte <nick@nickwhyte.com>
2018-12-31 10:49:12 +1100

    flake8

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ long_description = open(readme_path).read()
 
 try:
     version = get_version()
-except Exception as e:
+except Exception:
     version = '0.0.0-dev'
 
 setup(
@@ -30,7 +30,8 @@ setup(
     packages=['nessclient', 'nessclient.cli', 'nessclient.cli.server'],
     author="Nick Whyte",
     author_email='nick@nickwhyte.com',
-    description="Implementation/abstraction of the Ness D8x / D16x Serial Interface ASCII protocol",
+    description="Implementation/abstraction of the Ness D8x / D16x Serial "
+                "Interface ASCII protocol",
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/nickw444/nessclient',
