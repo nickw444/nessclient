da97bbeda7099628f5e4abffd4cbfef6028e6ba7
Nick Whyte <nick@nickwhyte.com>
2019-02-21 08:33:18 +1100

    WIP: packet tests

--- nessclient_tests/test_packet.py
+++ nessclient_tests/test_packet.py
@@ -3,6 +3,7 @@ import logging
 import unittest
 from os import path
 
+from nessclient import BaseEvent
 from nessclient.packet import Packet, CommandType
 
 _LOGGER = logging.getLogger(__name__)
@@ -15,7 +16,7 @@ def fixture_path(fixture_name: str):
 class PacketTestCase(unittest.TestCase):
     def testDecodeEncodeIdentity(self):
         cases = [
-            # b'8700036100070018092118370677',
+            # '8700036100070018092118370677',
             '8300c6012345678912EE7'
         ]
 
@@ -23,12 +24,48 @@ class PacketTestCase(unittest.TestCase):
             pkt = Packet.decode(case)
             self.assertEqual(case, pkt.encode())
 
-    def testDecodeAndEncode(self):
+    def testDecode(self):
         with open(fixture_path('sample_output.txt')) as f:
             for line in f.readlines():
                 line = line.strip()
                 pkt = Packet.decode(line)
                 _LOGGER.info("Decoded '%s' into %s", line, pkt)
+
+    def testDecodeForumCases(self):
+        cases = [
+            # FrontBottom
+            # https://community.home-assistant.io/t/ness-dx8-dx16-alarm/23285/28?u=nickw444
+            '8709036101050018122709413536',
+            '87098361000400181227094135b8',
+            '8709036101040018122709413735',
+            # aumatt
+            # https://community.home-assistant.io/t/ness-dx8-dx16-alarm/23285/35?u=nickw444
+            '8704036100120019012909332957',
+            # Glenn_Pettitt
+            # ValueError: Unable to consume all data
+            # https://community.home-assistant.io/t/ness-dx8-dx16-alarm/23285/38?u=nickw444
+            '820003600000001b',
+            '8200036014000007',
+            '820003600000001b',
+            # FrontBottom
+            # https://community.home-assistant.io/t/ness-dx8-dx16-alarm/23285/41?u=nickw444
+            '8709836100050019010509174800',
+            '8709836100050019010509174800',
+            # 2019-01-30 18:47:14 ERROR (MainThread) [homeassistant.core] Error doing job:
+            # Task exception was never retrieved ...
+            # type message, where the keepalive polling seems to crap itself?
+            # https://community.home-assistant.io/t/ness-dx8-dx16-alarm/23285/45?u=nickw444
+            '820003600001001a',
+            '820361230001f6',
+            '8300360S00E9',
+            # aumatt: Two packets it failed on
+            # https://community.home-assistant.io/t/ness-dx8-dx16-alarm/23285/47?u=nickw444
+            '8704036100140019012915060699',
+            '87048361001300190129231052b6',
+        ]
+        for case in cases:
+            pkt = Packet.decode(case)
+            _LOGGER.info("Decoded '%s' into %s", case, pkt)
 
     def testUserInterfacePacketDecode(self):
         pkt = Packet.decode('8300c6012345678912EE7')
@@ -89,3 +126,8 @@ class PacketTestCase(unittest.TestCase):
         self.assertEqual(pkt.data, '070000')
         self.assertIsNone(pkt.timestamp)
         # self.assertEqual(pkt.checksum, 0x14)
+
+    def testFoo(self):
+        pkt = Packet.decode('8200036015000006')
+        evt = BaseEvent.decode(pkt)
+        print(evt)
