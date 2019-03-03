d12f7e8eb1d66db08b0200f442567b2621f0d501
Nick Whyte <nick@nickwhyte.com>
2019-03-03 17:27:06 +1100

    .

--- nessclient_tests/test_client.py
+++ nessclient_tests/test_client.py
@@ -4,49 +4,54 @@ from asynctest import MagicMock
 from nessclient import Client
 from nessclient.connection import Connection
 
-pytestmark = pytest.mark.asyncio
-
 
 def get_data(pkt: bytes) -> bytes:
     return pkt[7:-4]
 
 
+@pytest.mark.asyncio
 async def test_arm_away(connection, client):
     await client.arm_away('1234')
     assert connection.write.call_count == 1
     assert get_data(connection.write.call_args[0][0]) == b'A1234E'
 
 
+@pytest.mark.asyncio
 async def test_arm_home(connection, client):
     await client.arm_home('1234')
     assert connection.write.call_count == 1
     assert get_data(connection.write.call_args[0][0]) == b'H1234E'
 
 
+@pytest.mark.asyncio
 async def test_disarm(connection, client):
     await client.disarm('1234')
     assert connection.write.call_count == 1
     assert get_data(connection.write.call_args[0][0]) == b'1234E'
 
 
+@pytest.mark.asyncio
 async def test_panic(connection, client):
     await client.panic('1234')
     assert connection.write.call_count == 1
     assert get_data(connection.write.call_args[0][0]) == b'*1234#'
 
 
+@pytest.mark.asyncio
 async def test_aux_on(connection, client):
     await client.aux(1, True)
     assert connection.write.call_count == 1
     assert get_data(connection.write.call_args[0][0]) == b'11*'
 
 
+@pytest.mark.asyncio
 async def test_aux_off(connection, client):
     await client.aux(1, False)
     assert connection.write.call_count == 1
     assert get_data(connection.write.call_args[0][0]) == b'11#'
 
 
+@pytest.mark.asyncio
 async def test_update(connection, client):
     await client.update()
     assert connection.write.call_count == 2
@@ -54,12 +59,14 @@ async def test_update(connection, client):
     assert get_data(connection.write.call_args_list[1][0][0]) == b'S14'
 
 
+@pytest.mark.asyncio
 async def test_send_command(connection, client):
     await client.send_command('ABCDEFGHI')
     assert connection.write.call_count == 1
     assert get_data(connection.write.call_args[0][0]) == b'ABCDEFGHI'
 
 
+@pytest.mark.asyncio
 async def test_send_command_2(connection, client):
     await client.send_command('FOOBARBAZ')
     assert connection.write.call_count == 1
@@ -67,6 +74,7 @@ async def test_send_command_2(connection, client):
     assert get_data(connection.write.call_args[0][0]) == b'FOOBARBAZ'
 
 
+@pytest.mark.asyncio
 async def test_close(connection, client):
     await client.close()
     assert connection.close.call_count == 1
