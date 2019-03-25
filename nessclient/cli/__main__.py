38a31535d82a9cdef9366e09415d597489e4447f
Nick Whyte <nick@nickwhyte.com>
2019-03-25 21:18:05 +1100

    Show version during debug

--- nessclient/cli/__main__.py
+++ nessclient/cli/__main__.py
@@ -1,6 +1,7 @@
 import logging
 
 import click
+import pkg_resources
 
 from .events import events
 from .send_command import send_command
@@ -8,13 +9,24 @@ from .server import server
 
 LOG_LEVELS = ['error', 'warning', 'info', 'debug']
 
+_LOGGER = logging.getLogger(__name__)
 
 @click.group()
 @click.option('--log-level', type=click.Choice(LOG_LEVELS), default='warning')
 def cli(log_level: str):
     level = getattr(logging, log_level.upper())
     logging.basicConfig(level=level)
+    _LOGGER.debug('nessclient version: %s', get_version())
 
+
+@cli.command()
+def version():
+    """Print installed package version."""
+    print(get_version())
+
+
+def get_version():
+    return pkg_resources.get_distribution('nessclient').version
 
 cli.add_command(events)
 cli.add_command(send_command)
