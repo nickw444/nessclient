c28e6c81d85fd385c50b0256900d9bcfa47dcdb9
Nick Whyte <nick@nickwhyte.com>
2023-06-09 21:30:34 +1000

    .

--- nessclient/cli/server/__init__.py
+++ nessclient/cli/server/__init__.py
@@ -4,6 +4,7 @@ from .alarm_server import AlarmServer
 
 DEFAULT_PORT = 65432
 
+
 @click.command(help="Run a dummy server")
 @click.option('--host', default='127.0.0.1')
 @click.option('--port', default=DEFAULT_PORT)
