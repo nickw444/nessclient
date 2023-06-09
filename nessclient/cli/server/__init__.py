d34512a9aa52b72636b709497efbaff3edcf0b69
Nick Whyte <nick@nickwhyte.com>
2023-06-09 21:31:30 +1000

    Share server default port with client (#48)

--- nessclient/cli/server/__init__.py
+++ nessclient/cli/server/__init__.py
@@ -2,10 +2,12 @@ import click
 
 from .alarm_server import AlarmServer
 
+DEFAULT_PORT = 65432
+
 
 @click.command(help="Run a dummy server")
 @click.option('--host', default='127.0.0.1')
-@click.option('--port', default=65432)
+@click.option('--port', default=DEFAULT_PORT)
 def server(host: str, port: int) -> None:
     s = AlarmServer(host=host, port=port)
     s.start()
