a472014c21f3f5c589e70ce119876d3f14d60669
Nick Whyte <nick@nickwhyte.com>
2024-08-15 22:20:58 +1000

    add serial tty to cli 2

--- nessclient/cli/send_command.py
+++ nessclient/cli/send_command.py
@@ -3,15 +3,21 @@ import asyncio
 import click
 
 from ..client import Client
+from .server import DEFAULT_PORT
 
 
 @click.command(help="Send a command")
-@click.option("--host", required=True)
-@click.option("--port", required=True, type=int)
+@click.option("--host", default="localhost")
+@click.option("--port", type=int, default=DEFAULT_PORT)
+@click.option("--serial-tty")
 @click.argument("command")
-def send_command(host: str, port: int, command: str) -> None:
+def send_command(host: str, port: int, serial_tty: str, command: str) -> None:
     loop = asyncio.get_event_loop()
-    client = Client(host=host, port=port)
+    client = Client(
+        host=host if serial_tty is None else None,
+        port=port if serial_tty is None else None,
+        serial_tty=serial_tty,
+    )
 
     loop.run_until_complete(client.send_command(command))
     loop.run_until_complete(client.close())
