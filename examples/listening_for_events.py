aef103c2fb0afe7680e87735268ff77742d0dc85
Joshua Leaper <poshernater@outlook.com>
2025-05-25 23:09:10 +0930

    Update example libraries to support past python 3.10

--- examples/listening_for_events.py
+++ examples/listening_for_events.py
@@ -2,9 +2,8 @@ import asyncio
 
 from nessclient import Client, ArmingState, BaseEvent
 
-loop = asyncio.get_event_loop()
-host = '127.0.0.1'
-port = 65432
+host = '172.0.0.1'
+port = 4999
 client = Client(host=host, port=port)
 
 
@@ -23,10 +22,13 @@ def on_event_received(event: BaseEvent):
     print('Event received:', event)
 
 
-loop.run_until_complete(asyncio.gather(
-    client.keepalive(),
-    client.update(),
-))
+async def main():
+    await asyncio.gather(
+        client.keepalive(),
+        client.update(),
+    )
+    await client.close()
 
-client.close()
-loop.close()
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- examples/sending_commands.py
+++ examples/sending_commands.py
@@ -1,24 +1,27 @@
 import asyncio
-
 from nessclient import Client
 
-loop = asyncio.get_event_loop()
-host = '127.0.0.1'
-port = 65432
-client = Client(host=host, port=port)
 
-# Send arming command via library abstraction
-loop.run_until_complete(client.arm_away('1234'))
-# Send panic command via library abstraction
-loop.run_until_complete(client.panic('1234'))
-# Send disarm command via library abstraction
-loop.run_until_complete(client.disarm('1234'))
-# Send aux control command for output 2 via library abstraction
-loop.run_until_complete(client.aux(2))
-# Send custom command
-# In this instance, we are sending a status update command to view
-# output status
-loop.run_until_complete(client.send_command('S15'))
+async def main():
+    host = '172.0.0.1'
+    port = 4999
+    client = Client(host=host, port=port)
 
-client.close()
-loop.close()
+    # Send arming command via library abstraction
+    await client.arm_away('1234')
+    # Send panic command via library abstraction
+    await client.panic('1234')
+    # Send disarm command via library abstraction
+    await client.disarm('1234')
+    # Send aux control command for output 2 via library abstraction
+    await client.aux(2)
+    # Send custom command
+    # In this instance, we are sending a status update command to view
+    # output status
+    await client.send_command('S15')
+
+    await client.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
