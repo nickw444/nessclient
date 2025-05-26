dc89dc3dc5f9224b367034a0737dc706072d5bbb
Joshua Leaper <poshernater@outlook.com>
2025-05-26 19:39:52 +0930

    Include PanelVersionUpdate class to __init__.py

--- nessclient/__init__.py
+++ nessclient/__init__.py
@@ -1,6 +1,6 @@
 from .client import Client
 from .alarm import ArmingState, ArmingMode
-from .event import BaseEvent
+from .event import BaseEvent, PanelVersionUpdate
 
-__all__ = ["Client", "ArmingState", "ArmingMode", "BaseEvent"]
+__all__ = ["Client", "ArmingState", "ArmingMode", "BaseEvent", "PanelVersionUpdate"]
 __version__ = "0.0.0-dev"
