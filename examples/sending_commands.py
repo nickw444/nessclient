c6eb0bd6a5eaba1409f62b1b7da345cbdab55191
Nick Whyte <nick@nickwhyte.com>
2025-08-26 20:26:11 +1000

    examples: enable send examples; add note about keepalive while awaiting SXX

--- examples/sending_commands.py
+++ examples/sending_commands.py
@@ -11,20 +11,20 @@ async def main() -> None:
     # Run background loops so responses are received while awaiting
     keepalive_task = asyncio.create_task(client.keepalive())
     try:
-        # # Send arming command via library abstraction
-        # await client.arm_away("1234")
-        # # Send panic command via library abstraction
-        # await client.panic("1234")
-        # # Send disarm command via library abstraction
-        # await client.disarm("1234")
-        # # Send aux control command for output 2 via library abstraction
-        # await client.aux(2)
-        # # Send custom command
-        # # In this instance, we are sending a status update command to view
-        # # output status
-        # await client.send_command("S15")
-
-        # Await a status request and receive the decoded response
+        # Send arming command via library abstraction
+        await client.arm_away("1234")
+        # Send panic command via library abstraction
+        await client.panic("1234")
+        # Send disarm command via library abstraction
+        await client.disarm("1234")
+        # Send aux control command for output 2 via library abstraction
+        await client.aux(2)
+        # Send custom command
+        # In this instance, we are sending a status update command to view
+        # output status
+        await client.send_command("S15")
+        # Send and await a status request to receive the decoded response
+        # note: the background keepalive must be running for this to work
         resp = await client.send_command_and_wait("S14", timeout=5.0)
         print("Arming status response:", resp)
     finally:
